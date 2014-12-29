f12d131641fb07fd4e48c72764929dbb5a322cf5
violetyk <yuhei.kagaya@gmail.com>
2014-12-30 02:05:42 +0900

    WEB+DB Press vol.82の第3章まで

---
+++ src/gosample/gosample.go
@@ -0,0 +1,16 @@
+package gosample
+
+// var Message string = "hello world"
+var Message string = `Hello
+world
+`
+
+var foo, bar, buz string = "foo", "bar", "buz"
+var (
+  a string = "aaa"
+  b        = "bbb"
+  c        = "ccc"
+  d        = "ddd"
+  e        = "eee"
+)
+
